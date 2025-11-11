7766e7dc144bc148991a00f3623368d55c6c1537
Map-Sol <useris6631@gmail.com>
2025-11-11 20:58:58 +0530

    Update vector.java

--- vector.java
+++ vector.java
@@ -1,61 +1,54 @@
+
 import java.util.*;
 
-public class DistanceVectorRouting {
+public class DVR {
     static final int INF = 9999;
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
+        Scanner s = new Scanner(System.in);
         System.out.print("Enter number of nodes: ");
-        int n = sc.nextInt();
-
-        int[][] cost = new int[n][n];
+        int n = s.nextInt();
+        int[][] c = new int[n][n];
         System.out.println("Enter cost matrix (use 9999 for no direct link):");
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                c[i][j] = s.nextInt();
+
+        int[][] d = new int[n][n];
+        int[][] nh = new int[n][n];
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                cost[i][j] = sc.nextInt();
-            }
-        }
-
-        int[][] dist = new int[n][n];
-        int[][] nextHop = new int[n][n];
-
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                dist[i][j] = cost[i][j];
-                if (cost[i][j] != INF && i != j)
-                    nextHop[i][j] = j;
+                d[i][j] = c[i][j];
+                if (c[i][j] != INF && i != j)
+                    nh[i][j] = j;
                 else
-                    nextHop[i][j] = -1;
+                    nh[i][j] = -1;
             }
+            d[i][i] = 0;
+            nh[i][i] = -1;
         }
 
-       
-        for (int k = 0; k < n - 1; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    for (int l = 0; l < n; l++) {
-                        if (dist[i][l] + dist[l][j] < dist[i][j]) {
-                            dist[i][j] = dist[i][l] + dist[l][j];
-                            nextHop[i][j] = nextHop[i][l];
+        for (int k = 0; k < n - 1; k++)
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    for (int l = 0; l < n; l++)
+                        if (d[i][l] < INF && d[l][j] < INF && d[i][l] + d[l][j] < d[i][j]) {
+                            d[i][j] = d[i][l] + d[l][j];
+                            nh[i][j] = nh[i][l];
                         }
-                    }
-                }
-            }
-        }
 
-       
         for (int i = 0; i < n; i++) {
             System.out.println("\nRouting table for Node " + i + ":");
             System.out.println("Destination\tCost\tNext Hop");
             for (int j = 0; j < n; j++) {
-                if (i == j)
-                    continue;
-                System.out.println(j + "\t\t" + dist[i][j] + "\t" + nextHop[i][j]);
+                if (i == j) continue;
+                String cs = (d[i][j] >= INF) ? "INF" : String.valueOf(d[i][j]);
+                String hs = (nh[i][j] == -1) ? "-" : String.valueOf(nh[i][j]);
+                System.out.println(j + "\t\t" + cs + "\t" + hs);
             }
         }
-
-        sc.close();
+        s.close();
     }
 }
+
